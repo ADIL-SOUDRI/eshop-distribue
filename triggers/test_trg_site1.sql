9a7ed03c63fbe775fc4dcc3cf5eaac9c5f9a9dca
Adil SOUDRI <adilsoudri@gmail.com>
2026-05-25 16:25:51 +0100

    Ignorer les dossiers et les fichiers inclus dans .gitignore

--- triggers/test_trg_site1.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO LigneCommandes
-VALUES (1,101,1001,120,10);
-
-SELECT 'Global' AS site, idlignecommande, quantite, remise FROM LigneCommandes
-UNION ALL
-SELECT 'Site1', idlignecommande, quantite, remise FROM LigneCommandes1
-UNION ALL
-SELECT 'Site2', idlignecommande, quantite, remise FROM LigneCommandes2@site2
-ORDER BY site, idlignecommande;
